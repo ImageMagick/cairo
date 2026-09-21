b89d9212c1e7f7058793f87b1f9f0768cd8e888f
Dirk Lemstra <dirk@lemstra.org>
2026-09-21 08:49:31 +0200

    Changes to cairo.

--- src/cairo.h
+++ src/cairo.h
@@ -50,10 +50,10 @@
 # define CAIRO_END_DECLS
 #endif
 
-#if (defined(_WIN32) || defined(__CYGWIN__)) && !defined(CAIRO_WIN32_STATIC_BUILD)
+#if (defined(_WIN32) || defined(__CYGWIN__)) && !defined (_LIB)
 # define _cairo_export __declspec(dllexport)
 # define _cairo_import __declspec(dllimport)
-#elif defined(__GNUC__)
+#elif defined(__GNUC__) && (__GNUC__ >= 4)
 # define _cairo_export __attribute__((__visibility__("default")))
 # define _cairo_import
 #else
