ae7c408bed8c955024f36bfd9c1563e2aceaa458
Dirk Lemstra <dirk@lemstra.org>
2026-09-21 08:53:54 +0200

    Added missing typecast to fix building on alpine.

--- src/cairo-image-source.c
+++ src/cairo-image-source.c
@@ -87,7 +87,7 @@ _pixman_transparent_image (void)
 	if (unlikely (image == NULL))
 	    return NULL;
 
-	if (_cairo_atomic_ptr_cmpxchg (&__pixman_transparent_image,
+	if (_cairo_atomic_ptr_cmpxchg ((void * _Atomic *) &__pixman_transparent_image,
 				       NULL, image))
 	{
 	    pixman_image_ref (image);
@@ -119,7 +119,7 @@ _pixman_black_image (void)
 	if (unlikely (image == NULL))
 	    return NULL;
 
-	if (_cairo_atomic_ptr_cmpxchg (&__pixman_black_image,
+	if (_cairo_atomic_ptr_cmpxchg ((void * _Atomic *) &__pixman_black_image,
 				       NULL, image))
 	{
 	    pixman_image_ref (image);
@@ -151,7 +151,7 @@ _pixman_white_image (void)
 	if (unlikely (image == NULL))
 	    return NULL;
 
-	if (_cairo_atomic_ptr_cmpxchg (&__pixman_white_image,
+	if (_cairo_atomic_ptr_cmpxchg ((void * _Atomic *) &__pixman_white_image,
 				       NULL, image))
 	{
 	    pixman_image_ref (image);
